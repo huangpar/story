5bbd2b42b7a7999d2da29537e92f4c9eec94e360
Parker Huang <parkerdhuang@gmail.com>
2026-01-16 13:19:55 -0800

    h

--- src/entertainment.js
+++ src/entertainment.js
@@ -34,11 +34,16 @@ export function Entertainment() {
 
     return (
         <div className="entertainment">
+            <div className="bg-layer">
+                <div className="bg-wash"></div>
+                <div className="orb orb-indigo"></div>
+                <div className="orb orb-purple"></div>
+            </div>
             <h1 className="header">
                 <div className="header-center">
-                    <Sparkles size={35} color="#EAB308" />
+                    {/* <Sparkles size={35} color="#EAB308" /> */}
                     <Link to="/"><span className="gradient-text">The Republic/Storia</span></Link>
-                    <Sparkles size={35} color="#EC4899" />
+                    {/* <Sparkles size={35} color="#EC4899" /> */}
                 </div>
                 <Link to="/entertainers" className="addPerson">
                     <Users className="users" size={25} color="#ffffffff" />
